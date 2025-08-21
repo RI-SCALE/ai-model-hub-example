eaa3e9b96c3bd9cc6e81e854e5713b2539270af4
Hugo Dettner Källander <hugokallander@gmail.com>
2025-08-21 17:46:46 +0200

    Fix artifact id

--- upload_model.py
+++ upload_model.py
@@ -6,21 +6,26 @@ import os
 import httpx
 import yaml
 from dotenv import load_dotenv
-from hypha_rpc import connect_to_server, login
-from hypha_rpc.rpc import RemoteException, RemoteService
+from hypha_rpc import connect_to_server, login  # type: ignore
+from hypha_rpc.rpc import RemoteException  # type: ignore
+from hypha_rpc.utils import ObjectProxy  # type: ignore
 
 logger = logging.getLogger(__name__)
 
 
 async def upload_directory(
-    artifact_manager: RemoteService, directory: str, artifact_id: str
+    artifact_manager: ObjectProxy, directory: str, artifact_id: str
 ):
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
-        put_file_url = await artifact_manager.put_file(
+        put_file_url = await artifact_manager.put_file(  # type: ignore
             artifact_id=artifact_id,
             file_path=filename,
         )
+
+        if not isinstance(put_file_url, str):
+            raise ValueError("Expected put_file_url to be a string")
+
         with open(file_path, "rb") as f:
             content = f.read()
             async with httpx.AsyncClient() as client:
@@ -32,49 +37,55 @@ async def upload_directory(
                 response.raise_for_status()
 
 
-async def upload_model(model_dir: str | None = None):
+async def upload_model(model_dir: str):
 
     load_dotenv()
 
     server_url = os.getenv("HYPHA_SERVER_URL") or "https://hypha.aicell.io"
-    token = os.getenv("HYPHA_TOKEN") or await login({"server_url": server_url})
+    token: str = os.getenv("HYPHA_TOKEN") or await login({"server_url": server_url})  # type: ignore
+    workspace = "ri-scale"
 
-    server_config = {
+    server_config: dict[str, str] = {
         "server_url": server_url,
         "token": token,
     }
 
-    async with connect_to_server(server_config) as server:
-        artifact_manager: RemoteService = await server.get_service(  # type: ignore
+    async with connect_to_server(server_config) as server:  # type: ignore
+        artifact_manager: ObjectProxy = await server.get_service(  # type: ignore
             "public/artifact-manager"
         )
+
+        if not isinstance(artifact_manager, ObjectProxy):
+            raise ValueError("Expected artifact_manager to be a ObjectProxy")
 
         with open(os.path.join(model_dir, "manifest.yaml"), "r") as f:
             model_manifest = yaml.safe_load(f)
 
         model_id = model_manifest["id"]
+        full_parent_id = f"{workspace}/ai-model-hub"
+        full_artifact_id = f"{workspace}/{model_id}"
 
         try:
-            await artifact_manager.create(
-                parent_id="ri-scale/ai-model-hub",
-                workspace="ri-scale",
+            await artifact_manager.create(  # type: ignore
+                parent_id=full_parent_id,
+                workspace=workspace,
                 alias=model_id,
                 stage=True,
                 manifest=model_manifest,
             )
         except RemoteException:
             print("Artifact already exists.")
-            await artifact_manager.edit(
-                artifact_id=f"ri-scale/{model_id}", manifest=model_manifest, stage=True
+            await artifact_manager.edit(  # type: ignore
+                artifact_id=full_artifact_id, manifest=model_manifest, stage=True
             )
 
         await upload_directory(
             artifact_manager=artifact_manager,
             directory=model_dir,
-            artifact_id=model_id,
+            artifact_id=full_artifact_id,
         )
 
-        # await artifact_manager.commit(artifact_id=model_id)
+        # await artifact_manager.commit(artifact_id=full_artifact_id)
 
 
 def main():
